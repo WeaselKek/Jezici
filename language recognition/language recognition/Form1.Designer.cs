c6f44ce995c7a65b7e5dd67d80272a9bdc791fdf
WeaselKek <pilgrimdestroyer@gmail.com>
2018-02-08 22:59:04 +0100

    dif

---
+++ language recognition/language recognition/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace language_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMain = new System.Windows.Forms.RichTextBox();
+            this.btnMain = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbMain
+            // 
+            this.rtbMain.Location = new System.Drawing.Point(26, 54);
+            this.rtbMain.Name = "rtbMain";
+            this.rtbMain.Size = new System.Drawing.Size(654, 211);
+            this.rtbMain.TabIndex = 0;
+            this.rtbMain.Text = "";
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(88, 310);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(542, 58);
+            this.btnMain.TabIndex = 1;
+            this.btnMain.Text = "Prepoznaj";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMain);
+            this.Controls.Add(this.rtbMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMain;
+        private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ language recognition/language recognition/Form1.cs
@@ -0,0 +1,22 @@
+﻿using LanguageIdentification;
+using System;
+using System.Windows.Forms;
+
+namespace language_recognition
+{
+    public partial class Form1 : Form
+    {
+        private LanguageIdentifier lan;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lan = LanguageIdentifier.New("langprofiles-char-1_5-nfc-all.bin.gz", "Vector", -1);
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            label1.Text = lan.Identify(rtbMain.Text);
+        }
+    }
+}
