8b23cd327b8ac67f6c5e48778855ee66e5bc3580
WeaselKek <pilgrimdestroyer@gmail.com>
2018-02-09 03:54:53 +0100

    commie

--- language recognition/language recognition/Form1.Designer.cs
+++ language recognition/language recognition/Form1.Designer.cs
@@ -30,46 +30,63 @@
         {
             this.rtbMain = new System.Windows.Forms.RichTextBox();
             this.btnMain = new System.Windows.Forms.Button();
+            this.lblJezik = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // rtbMain
             // 
+            this.rtbMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtbMain.Location = new System.Drawing.Point(26, 54);
             this.rtbMain.Name = "rtbMain";
-            this.rtbMain.Size = new System.Drawing.Size(654, 211);
+            this.rtbMain.Size = new System.Drawing.Size(664, 293);
             this.rtbMain.TabIndex = 0;
             this.rtbMain.Text = "";
             // 
             // btnMain
             // 
-            this.btnMain.Location = new System.Drawing.Point(88, 310);
+            this.btnMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.Location = new System.Drawing.Point(80, 369);
             this.btnMain.Name = "btnMain";
             this.btnMain.Size = new System.Drawing.Size(542, 58);
             this.btnMain.TabIndex = 1;
-            this.btnMain.Text = "Prepoznaj";
+            this.btnMain.Text = "Identify";
             this.btnMain.UseVisualStyleBackColor = true;
             this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // lblJezik
+            // 
+            this.lblJezik.AutoSize = true;
+            this.lblJezik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJezik.Location = new System.Drawing.Point(113, 22);
+            this.lblJezik.Name = "lblJezik";
+            this.lblJezik.Size = new System.Drawing.Size(51, 20);
+            this.lblJezik.TabIndex = 2;
+            this.lblJezik.Text = "label1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 391);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Language:";
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnMain;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(725, 439);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblJezik);
             this.Controls.Add(this.btnMain);
             this.Controls.Add(this.rtbMain);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Language Recognition Software";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +96,7 @@
 
         private System.Windows.Forms.RichTextBox rtbMain;
         private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Label lblJezik;
         private System.Windows.Forms.Label label1;
     }
 }
--- language recognition/language recognition/Form1.cs
+++ language recognition/language recognition/Form1.cs
@@ -1,22 +1,41 @@
 ﻿using LanguageIdentification;
 using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace language_recognition
 {
     public partial class Form1 : Form
     {
         private LanguageIdentifier lan;
+        private Dictionary<string, string> dict;
 
         public Form1()
         {
-            InitializeComponent();
-            lan = LanguageIdentifier.New("langprofiles-char-1_5-nfc-all.bin.gz", "Rank,sf", -1);
+            InitializeComponent();     
         }
 
         private void btnMain_Click(object sender, EventArgs e)
         {
-            label1.Text = lan.Identify(rtbMain.Text);
+            String id;
+            id = lan.Identify(rtbMain.Text);
+
+            lblJezik.Text = dict[id];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblJezik.Text = "";
+            lan = LanguageIdentifier.New("profil_2.gz", "Rank,sf", -1);
+            dict = new Dictionary<string, string>();
+            dict["en"] = "English";
+            dict["es"] = "Spanish";
+            dict["fr"] = "French";
+            dict["it"] = "Italian";
+            dict["ru"] = "Russian";
+            dict["sr"] = "Serbian";
+            dict["de"] = "German";
+            dict["pt"] = "Portuguese";
         }
     }
 }
